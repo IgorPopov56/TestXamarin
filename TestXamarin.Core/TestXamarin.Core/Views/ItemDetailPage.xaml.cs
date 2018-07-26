ffcec038115454bd143cfd93d58d9fcc79fccd90
Igor <igorp@cpart.co.il>
2018-07-26 14:08:41 +0300

    init

---
+++ TestXamarin.Core/TestXamarin.Core/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using TestXamarin.Core.Models;
+using TestXamarin.Core.ViewModels;
+
+namespace TestXamarin.Core.Views {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ItemDetailPage : ContentPage {
+        ItemDetailViewModel viewModel;
+
+        public ItemDetailPage(ItemDetailViewModel viewModel) {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+        public ItemDetailPage() {
+            InitializeComponent();
+
+            var item = new Item {
+                Text = "Item 1",
+                Description = "This is an item description."
+            };
+
+            viewModel = new ItemDetailViewModel(item);
+            BindingContext = viewModel;
+        }
+    }
+}
